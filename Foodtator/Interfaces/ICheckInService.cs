fc7414abaf64a8ad4b197d5d3ecf72fd7cd13456
FisherRobison <fisherrobison@gmail.com>
2016-06-29 17:23:02 -0700

    Added CheckInService Interface

---
+++ Foodtator/Interfaces/ICheckInService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YelpSharp.Data;
+
+namespace Foodtator.Interfaces
+{
+   public interface ICheckInService
+    {
+        int EstablishmentSelected(Business model);
+    }
+}
--- Foodtator/Services/CheckInService.cs
+++ Foodtator/Services/CheckInService.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using YelpSharp.Data;
 
 namespace Foodtator.Services
 {
-    public class CheckInService
+    public class CheckInService : BaseService
     {
+        public int EstablishmentSelected(Business model)
+        {
+            int uid = 0;
+
+            DataProvider.ExecuteNonQuery(GetConnection, "Establishment_Selected"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+               {
+                   paramCollection.AddWithValue("@UserId", UserService.GetCurrentUserId());
+                   paramCollection.AddWithValue("@EstablishmentName", model.name);
+                   paramCollection.AddWithValue("@Latitude", model.location.coordinate.latitude);
+                   paramCollection.AddWithValue("@Latitude", model.location.coordinate.longitude);
+                   paramCollection.AddWithValue("@ImageUrl", model.image_url);
+
+                   SqlParameter p = new SqlParameter("@Id", System.Data.SqlDbType.Int);
+                   p.Direction = System.Data.ParameterDirection.Output;
+                   paramCollection.Add(p);
+
+               }, returnParameters: delegate (SqlParameterCollection param)
+               {
+                   int.TryParse(param["@Id"].Value.ToString(), out uid);
+               });
+
+
+            return uid;
+        }
+
+
+
 
     }
 }
