510803903800f93ec43d5c906e47c33b2bc69425
Tom Liu <gt3240@me.com>
2016-07-02 13:53:49 -0700

    log out fixed

--- Foodtator/Controllers/Api/UserApiController.cs
+++ Foodtator/Controllers/Api/UserApiController.cs
@@ -70,20 +70,7 @@ namespace Foodtator.Controllers.Api
             else
             {
                 bool response = UserService.Signin(model.email, model.password);
-
-                if (response == true)
-                {
-                    ItemResponse<Domain.UserDetails> newResponse = new ItemResponse<Domain.UserDetails>();
-                    string userId = UserService.GetCurrentUserId();
-                    Guid userGuid = new Guid(userId);
-                    newResponse.Item = UserService.GetUserById(userGuid);
-                    return Request.CreateResponse(newResponse);
-
-                }
-                else
-                {
-                    return Request.CreateResponse(response);
-                }
+                return Request.CreateResponse(response);
             }
         }
 
