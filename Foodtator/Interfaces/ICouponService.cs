7a58508e4b8c5a4f141ed63db634487567960145
FisherRobison <fisherrobison@gmail.com>
2016-07-07 15:08:22 -0700

    Coupon Mid-Tier, Template and SProc good to go

---
+++ Foodtator/Interfaces/ICouponService.cs
@@ -0,0 +1,16 @@
+﻿using Foodtator.Models.RequestModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodtator.Interfaces
+{
+    public interface ICouponService
+    {
+        int CreateCoupon(CouponRequestModel model);
+        List<Domain.Coupon> GetCouponsByUserID();
+        int UpdateCoupon(CouponRequestModel model);
+    }
+}
---
+++ Foodtator/Models/RequestModel/CouponRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foodtator.Models.RequestModel
+{
+    public class CouponRequestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CouponCode { get; set; }
+        public string Description { get; set; }
+        public DateTime Expires { get; set; }
+        public int MaxRedemptions { get; set; }
+
+    }
+}
