aba05d8e974db0ecf95cd94a3306d2d9bfc801cd
Tom Liu <gt3240@me.com>
2016-07-09 00:18:48 -0700

    changed up dashboard a little..

---
+++ Foodtator/Scripts/FoodTator/App/Biz/Controllers/profileController.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .controller('profileController', ProfileController);
+
+    ProfileController.$inject = ['$scope'];
+
+    function ProfileController(
+         $scope
+        ) {
+
+        var vm = this;
+        vm.$scope = $scope;
+
+        /****  Variables ***/
+
+
+
+        /****  Functions ***/
+
+        init();
+
+        function init() {
+            console.log("ProfileController loaded");
+
+
+        }
+
+
+    }
+})();
---
+++ Foodtator/Scripts/FoodTator/Modules/AngularDropzone.js
@@ -0,0 +1,4 @@
+﻿(function ($tkj) {
+    $tkj.moduleOptions.extraModuleDependencies.push("ngDropzone");
+
+}(tkj))
